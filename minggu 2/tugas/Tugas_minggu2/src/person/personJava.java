f3d9f8e19ab8ac88994343c0ecdb4d1cf59b27d5
anamade <50694980+anamade@users.noreply.github.com>
2021-03-16 08:55:01 +0700

    frist commit

---
+++ minggu 2/tugas/Tugas_minggu2/src/person/personJava.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author lenovo
+ */
+public class personJava {
+
+    public static void main(String[] args) {
+
+        String fName, Name, suId, stuStatus;
+        fName = "M.KHOLIKUL ANAM";
+        Name = "ADE KURNIADI";
+        suId = "E41202189";
+        stuStatus = "Active";
+        
+      
+        System.out.println("Student Name\t:" + fName + "" + Name);
+        System.out.println("Student ID\t:" + suId);
+        System.out.println("Student Status\t:" + stuStatus);
+
+    }
+}
